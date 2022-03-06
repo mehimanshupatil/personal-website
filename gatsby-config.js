a5ded4fe4034a82f518f3088a844dc33fdb4347c
Himanshu Patil <himanshupatil744@gmail.com>
2022-03-06 19:38:05 +0530

    init

---
+++ gatsby-config.js
@@ -0,0 +1,157 @@
+require(`dotenv`).config();
+
+const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE;
+
+module.exports = {
+  siteMetadata: {
+    // You can overwrite values here that are used for the SEO component
+    // You can also add new values here to query them like usual
+    // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.js
+    siteTitle: `Himanshu Patil`,
+    siteTitleAlt: `Himanshu Patil - Personal Website`,
+    siteHeadline: `Himanshu Patil - Personal Website`,
+    siteUrl: `https://himanshupatil.dev`,
+    siteDescription: `Personal Blog and Website of Himanshu Patil`,
+    siteLanguage: `en`,
+    siteImage: `/android-chrome-512x512.png`,
+    author: `@mehimanshupatil`,
+  },
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-minimal-blog`,
+      // See the theme's README for all available options
+      options: {
+        navigation: [
+          {
+            title: `Blog`,
+            slug: `/blog`,
+          },
+          {
+            title: `About`,
+            slug: `/about`,
+          },
+        ],
+        externalLinks: [
+          {
+            name: `Twitter`,
+            url: `https://twitter.com/mehimanshupatil`,
+          },
+          {
+            name: `Instagram`,
+            url: `https://instagram.com/mehimanshupatil`,
+          },
+          {
+            name: `Github`,
+            url: `https://github.com/mehimanshupatil`,
+          },
+          {
+            name: `Linkedin`,
+            url: `https://linkedin.com/mehimanshupatil`,
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-omni-font-loader`,
+      options: {
+        enableListener: true,
+        preconnect: [`https://fonts.gstatic.com`],
+        interval: 300,
+        timeout: 30000,
+        // If you plan on changing the font you'll also need to adjust the Theme UI config to edit the CSS
+        // See: https://github.com/LekoArts/gatsby-themes/tree/main/examples/minimal-blog#changing-your-fonts
+        web: [
+          {
+            name: `IBM Plex Sans`,
+            file: `https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;500;600;700&display=swap`,
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Himanshu Patil - Personal Website`,
+        short_name: `Himanshu Patil - Personal Website`,
+        description: `Personal Blog and Website of Himanshu Patil`,
+        start_url: `/`,
+        background_color: `#fff`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#6B46C1`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title: siteTitle
+                description: siteDescription
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allPost } }) =>
+              allPost.nodes.map((post) => {
+                const url = site.siteMetadata.siteUrl + post.slug;
+                const content = `<p>${post.excerpt}</p><div style="margin-top: 50px; font-style: italic;"><strong><a href="${url}">Keep reading</a>.</strong></div><br /> <br />`;
+
+                return {
+                  title: post.title,
+                  date: post.date,
+                  excerpt: post.excerpt,
+                  url,
+                  guid: url,
+                  custom_elements: [{ 'content:encoded': content }],
+                };
+              }),
+            query: `
+              {
+                allPost(sort: { fields: date, order: DESC }) {
+                  nodes {
+                    title
+                    date(formatString: "MMMM D, YYYY")
+                    excerpt
+                    slug
+                  }
+                }
+              }
+            `,
+            output: `rss.xml`,
+            title: `Personal Blog and Website of Himanshu Patil`,
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-gatsby-cloud`,
+    shouldAnalyseBundle && {
+      resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
+      options: {
+        analyzerMode: `static`,
+        reportFilename: `_bundle.html`,
+        openAnalyzer: false,
+      },
+    },
+  ].filter(Boolean),
+};
---
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/header-external-links.tsx
@@ -0,0 +1,24 @@
+/** @jsx jsx */
+import useMinimalBlogConfig from '@lekoarts/gatsby-theme-minimal-blog/src/hooks/use-minimal-blog-config';
+import * as React from 'react';
+import { jsx, Link as TLink } from 'theme-ui';
+
+const HeaderExternalLinks = () => {
+  const { externalLinks } = useMinimalBlogConfig();
+
+  return (
+    <React.Fragment>
+      {externalLinks && externalLinks.length > 0 && (
+        <div sx={{ 'a:not(:first-of-type)': { ml: 3 }, fontSize: [1, `18px`] }}>
+          {externalLinks.map((link) => (
+            <TLink key={link.url} href={link.url}>
+              {link.name}
+            </TLink>
+          ))}
+        </div>
+      )}
+    </React.Fragment>
+  );
+};
+
+export default HeaderExternalLinks;
